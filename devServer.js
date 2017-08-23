8025181c23cb6a2085c82282091695d67667691d
zsheill7 <zsheill7@gmail.com>
2017-08-23 13:20:54 -0700

    refactored referenced files

--- devServer.js
+++ devServer.js
@@ -1,29 +1,33 @@
-var path = require('path')
-var express = require('express')
-var webpack = require('webpack')
-var config = require('./webpack.config.dev')
+var path = require("path");
+var express = require("express");
+require("./services/passport");
 
-var app = express()
-var compiler = webpack(config)
+var webpack = require("webpack");
+var config = require("./webpack.config.dev");
+
+var app = express();
+require("./routes/authRoutes")(app);
+
+var compiler = webpack(config);
 
 app.use(
-  require('webpack-dev-middleware')(compiler, {
+  require("webpack-dev-middleware")(compiler, {
     noInfo: true,
     publicPath: config.output.publicPath
   })
-)
+);
 
-app.use(require('webpack-hot-middleware')(compiler))
+app.use(require("webpack-hot-middleware")(compiler));
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'))
-})
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
-app.listen(3030, 'localhost', function(err) {
+app.listen(3030, "localhost", function(err) {
   if (err) {
-    console.log(err)
-    return
+    console.log(err);
+    return;
   }
 
-  console.log('Listening at http://localhost:3030')
-})
+  console.log("Listening at http://localhost:3030");
+});
