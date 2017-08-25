ee29cfde1a0142ab53681ade62ec88a45937db83
zsheill7 <zsheill7@gmail.com>
2017-08-24 20:35:51 -0700

    added env variables

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+//keys.js
+if (process.env.NODE_ENV === "production") {
+  //return prod set of keys
+  module.exports = require("./prod");
+} else {
+  //return dev set of keys
+  module.exports = require("./dev");
+}
