662dc311809738a343f27fcc2cadeda882eaa6cb
zsheill7 <zsheill7@gmail.com>
2017-08-26 22:37:57 -0700

    added client

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Yo fam</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a href="http://localhost:5000/auth/google">Sign In With Google</a>
+      </div>
+    );
+  }
+}
+
+export default App;
